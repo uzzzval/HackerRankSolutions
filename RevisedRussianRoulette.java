5322c6209504abe82acc14d444e31772793c6184
Ujjawal Sharma <uzzzval@gmail.com>
2018-02-06 06:41:40 -0500

    Create RevisedRussianRoulette.java

---
+++ RevisedRussianRoulette.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] revisedRussianRoulette(int[] doors) {
+        // Complete this function
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] doors = new int[n];
+        for(int doors_i = 0; doors_i < n; doors_i++){
+            doors[doors_i] = in.nextInt();
+        }
+        int[] result = revisedRussianRoulette(doors);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+}
