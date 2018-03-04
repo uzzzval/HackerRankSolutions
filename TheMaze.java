1a73ea1c194ffae509a64c235762866ee2b66bd3
Ujjawal Sharma <uzzzval@gmail.com>
2018-03-04 06:55:50 -0500

    Update TheMaze.java

--- TheMaze.java
+++ TheMaze.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class Solution {
     public static void main(String args[] ) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-       
 		Scanner reader=new Scanner(System.in);
 		int rows=Integer.parseInt(reader.nextLine());
 		
